d61d798a3e629250237c79ee6d3ec1c84f956dd7
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-02-12 20:36:42 +0000

    Added more to the board

--- tic-tac-toe/board.py
+++ tic-tac-toe/board.py
@@ -1,10 +1,20 @@
 from enum import StrEnum
+from dataclasses import dataclass
+from functools import cached_property
 
 
+# Class to represent each slot in the board
+class BoardPosition(StrEnum):
+    Empty = " "
+    Ex = "X"
+    Oh = "O"
+
+
+@dataclass(frozen=True)
 class TicTacToeBoard:
-    def __init__(self):
-        self._board = [BoardPosition.Empty] * 9
+    board: BoardPosition = [BoardPosition.Empty] * 9
 
+    @cached_property
     def __str__(self):
         # Return a version of the board that can be printed nicely
         return ("-------------\n"
@@ -15,42 +25,47 @@ class TicTacToeBoard:
                 f"| {self._board[6]} | {self._board[7]} | {self._board[2]} |\n"
                 "----7---8---9")
 
-    def __get_child(index, value):
-        new = TicTacToeBoard()
-        new._board[index] = value
-        return new
+    def __get_child(self, index, value):
+        new_position = self.board
+        new_position[index] = value
+        return TicTacToeBoard(new_position)
 
     # Return a score showing which side this position favours
     # This is a simple heuristic function as we can explore the whole tree
     # We can return whether each side has won rather than a decimal
     # 1 favours X and -1 favours O
     def heuristic(self):
-        # Check horizontal and vertical
         for i in range(3):
+            # Check horizontal
             if self._board[3 * i] == self._board[3 * i + 1] == self._board[3 * i + 1]:
-                if self._board[3 + i] == BoardPosition.X:
+                if self._board[3 + i] == BoardPosition.Ex:
                     return 1
-                if self._board[3 + i] == BoardPosition.O:
+                if self._board[3 + i] == BoardPosition.Oh:
+                    return -1
+            # Check vertical
+            if self._board[3 * i] == self._board[3 * i + 1] == self._board[3 * i + 1]:
+                if self._board[3 + i] == BoardPosition.Ex:
+                    return 1
+                if self._board[3 + i] == BoardPosition.Oh:
                     return -1
         # Check diagonals
         if self._board[0] == self._board[4] == self._board[8] or \
                 self._board[2] == self._board[4] == self._board[6]:
-            if self._board[3 + i] == BoardPosition.X:
+            if self._board[3 + i] == BoardPosition.Ex:
                 return 1
-            if self._board[3 + i] == BoardPosition.O:
+            if self._board[3 + i] == BoardPosition.Oh:
                 return -1
 
         # No side has won
         return 0
 
     def move(self, index):
-        canMove = self._board[index] == BoardPosition.Empty
+        # Fail fast
+        assert index >= 0 and index <= 8
+
+        # Return a child board of the new position if possible
+        canMove = self.board[index] == BoardPosition.Empty
         if canMove:
-            pass
-
-
-# Class to represent each slot in the board
-class BoardPosition(StrEnum):
-    Empty = " "
-    X = "X"
-    O = "O"
+            return (True, self.__get_child(index))
+        else:
+            return (False, None)
